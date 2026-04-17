05945a3ea5e5d2c9ca46e228a7401529634f880d
Kolja Wawrowsky <3075215+apocryphx@users.noreply.github.com>
2026-04-16 19:46:23 -0700

    Initial Commit

---
+++ ES-Memory-Bridge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ES-Memory-Bridge
+//
+//  Created by Kolja Wawrowsky on 4/16/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
